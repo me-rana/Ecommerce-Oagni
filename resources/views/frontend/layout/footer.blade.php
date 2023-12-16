834e08cef9ad2bcfed0c5afcfd9f051b5c228478
Rana Bepari <irana.bpr9@gmail.com>
2023-12-16 11:52:42 +0600

    Version 2.0.1(Stable)

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -33,7 +33,7 @@
             <div class="col-lg-12">
                 <div class="footer__copyright">
                     <div class="footer__copyright__text"><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy; @php try { echo $settings->copyright;} catch (\Exception $e) {echo "MERANA";}@endphp - <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy; {{ $settings->Copyright ?? 'Rana Bepari' }} - <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
                     <div class="footer__copyright__payment"><img src="../assets/img/payment-item.png" alt=""></div>
                 </div>
